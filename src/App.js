639fd48ae92d4e7405aba2066d3f61fec8bd0112
TAMWO FEUWO FRANCK VALERE <valeretamwo@gmail.com>
2023-02-15 19:53:39 +0100

    1er commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState,useEffect} from 'react'
 
 function App() {
+
+   const [tasks, setTask] = useState([])
+  const [auth, setAuth] = useState(false)
+  
+  const authUser = () => {
+   fetch('http://localhost:8000/spotify/is-authenticated')
+      .then((Response) => Response.json())
+      .then((data) => {
+        setAuth(data.status)
+        console.log(data.status)
+        if (data.status===false){
+          fetch('http://localhost:8000/spotify/get-auth-url')
+            .then((response) =>  response.json() )
+            .then((data) => {
+            window.location.replace(data.url)
+              // console.log(data)
+          })
+        }
+      })
+  }
+  
+  const FetchTodo = () => {
+
+    fetch('http://localhost:8000/api/todolist/')
+      .then((res) => res.json())
+    .then((data)=>{setTask(data)})
+  }
+  
+  useEffect(() => {
+    authUser()
+    FetchTodo()
+  }, []);
+
+
   return (
+
+    
+
     <div className="App">
-      <header className="App-header">
+      
+      {auth &&
+        <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,6 +58,7 @@ function App() {
           Learn React
         </a>
       </header>
+      }
     </div>
   );
 }
