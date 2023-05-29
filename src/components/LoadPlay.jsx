be346647f19ce1f3c2292462077a0b72d4de25f9
TAMWO FEUWO FRANCK VALERE <valeretamwo@gmail.com>
2023-05-29 21:54:36 +0100

    ajout d'image2

--- src/components/LoadPlay.jsx
+++ src/components/LoadPlay.jsx
@@ -5,21 +5,27 @@ import { useContext, useEffect, useState } from 'react'
 import { ContextData } from '../contexts/dataContext'
 import { pexels } from './data'
 function LoadPlay(props) {
+    const [data,setData]=useState({"index":5,"playlist_name":''})
     let index = 5;
-    let playlist = [{"track_name":""}];
+    let playlist = {"playlist_name":""};
     const { popular, fetchData_all } = useContext(ContextData);
     useEffect(() => {
         fetchData_all();
-
-    }, [])
-    
-    if (props.id) {
+        if (props.id) {
             
-        playlist = popular.filter(p => p.playlist_id === props.id)[0];
-        index = popular.findIndex(p => p.playlist_id === props.id);
-        console.log(playlist);
+            setData({
+                "index":popular.findIndex(p => p.playlist_id === props.id),
+                "playlist_name": popular.filter(p => p.playlist_id === props.id)[0].playlist_name,
+
+            });
+            
+        
+        console.log(data);
     
     }
+    }, [])
+    
+    
     return (
         <>
              {/* <div className=' col-12 col justify-content-end d-flex'>
@@ -29,14 +35,14 @@ function LoadPlay(props) {
             <div className='container-fluid border-bottom border-light border-1 mb-2 '>
                 <div className='row'>
                     <div className='col-3 p-2'>
-                        <img className='img-fluid' src={pexels[index].src.tiny || arbre}  alt='playlist'/>
+                        <img className='img-fluid' src={pexels[data.index].src.tiny || arbre}  alt='playlist'/>
                     </div>
                     <div className='col-9 p-3'>
                         <p className='text-light' style={{fontSize:'10px'}} >
                             PLAYLIST
                         </p>
                         <p className='fw-bold text-light' style={{fontSize:'8vh'}}>
-                            {playlist.playlist_name||props.name || 'Fresh Breeze'}
+                            {data.playlist_name||props.name || 'Fresh Breeze'}
                         </p>
                         <p className='text-light' style={{fontSize:'10px'}}>valere tamwo</p>
                     </div>
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -50,7 +50,7 @@ function Player({currentSong,audio,isplaying,setIsplaying,songs,setCurrentSong})
                   </div> 
                   <div className='col-12 '>
                       <div className='d-flex justify-content-center gap-3'>
-                          <span>
+                          <span className='mt-3'>
                               <button className='btn mb-2' onClick={prevSong}>
                                   <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="white" class="bi bi-caret-left-square-fill" viewBox="0 0 16 16">
                                     <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm10.5 10V4a.5.5 0 0 0-.832-.374l-4.5 4a.5.5 0 0 0 0 .748l4.5 4A.5.5 0 0 0 10.5 12z"/>
@@ -81,7 +81,7 @@ function Player({currentSong,audio,isplaying,setIsplaying,songs,setCurrentSong})
                               }
                           </span>
                           
-                          <span>
+                          <span className='mt-3'>
                               <button className='btn' onClick={nextSong}>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="white" class="bi bi-caret-right-square-fill" viewBox="0 0 16 16">
                                  <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.5 10a.5.5 0 0 0 .832.374l4.5-4a.5.5 0 0 0 0-.748l-4.5-4A.5.5 0 0 0 5.5 4v8z"/>
